5e4fdda82f97be0923ff88e705bd10322fe9731f
Samuel Obe <obes@rpi.edu>
2020-06-06 22:19:59 -0400

    Second commit

---
+++ lib/secondscreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatefulWidget {
+  SecondScreen({Key key}) : super(key: key);
+
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(child: Text('Second Screen')),
+    );
+  }
+}
